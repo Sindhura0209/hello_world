109f014f32c53d73b56588dfc7823d717d2f8c37
sindhura0209 <snp120@miami.edu>
2025-09-09 09:37:43 -0400

    My last week script

---
+++ Trial Git and commit.R
@@ -0,0 +1,19 @@
+#Build a simple graph
+
+#Load Packages
+library(ggplot2)
+library(EVR628tools)
+
+#Load data
+data("data_heatwaves")
+
+#Build a plot
+p<- ggplot(data = data_heatwaves,
+           mapping = aes(x=year, y=temp_mean,
+                         group = paste(fishery, eu_rnpa))) +
+  
+geom_line()
+
+#Export my plot
+ggsave(plot = p,
+       filename = "results/img/my_first_plot.png")
